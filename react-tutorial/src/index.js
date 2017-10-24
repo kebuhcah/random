3a955559ac49873ce439c80654e8e36eaa765087
Kevin Sun <kevsun1@gmail.com>
2017-10-23 22:29:07 -0400

    add TODOs

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -137,6 +137,15 @@ function calculateWinner(squares) {
   return null;
 }
 
+/* 
+  TODO:
+  1. Display the move locations in the format “(1, 3)” in the move list.
+  2. Bold the currently selected item in the move list.
+  3. Rewrite Board to use two loops to make the squares instead of hardcoding them.
+  4. Add a toggle button that lets you sort the moves in either ascending or descending order.
+  5. When someone wins, highlight the three squares that caused the win.
+*/
+
 // ========================================
 
 ReactDOM.render(
